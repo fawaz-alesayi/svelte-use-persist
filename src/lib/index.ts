1163a7fb489e9f34731c665b8d07b9cc8914034e
fawaz-alesayi <fawaztsa@gmail.com>
2023-03-03 14:07:31 +0300

    refactor things so they don't use side-effects

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -25,40 +25,12 @@ export function persist(node: HTMLElement, {
     }) {
     const _key = key || node.id
     const _store = store || persisted(_key, {})
-    const load_cached_values = (store: Writable<any>) => {
-        for (const [key, value] of Object.entries(get(store))) {
-            const inputs = node.querySelectorAll(`[name="${key}"]`)
-            for (const input of inputs) {
-                if (input instanceof HTMLInputElement || input instanceof HTMLSelectElement) {
-                    if (input instanceof HTMLInputElement && input.type === 'radio') {
-                        input.checked = input.value === value
-                    } else if (input instanceof HTMLSelectElement) {
-                        input.selectedIndex = value as number
-                    } else {
-                        // @ts-ignore
-                        input.value = value
-                    }
-                }
-            }
-        }
-    }
-
-    load_cached_values(_store)
-
-    function save_form_state(event: Event, store: Writable<any>) {
-        const input = event.target as HTMLInputElement
-        const value = get(store)
-        if (input instanceof HTMLSelectElement) {
-            store.set({ ...value, [input.name]: input.selectedIndex })
-        } else if (input instanceof HTMLInputElement) {
-            store.set({ ...value, [input.name]: input.value })
-        }
-    }
 
     function handler(event: Event) {
         save_form_state(event, _store)
     }
 
+    load_cached_values(node, _store)
 
     node.addEventListener(persistOn, handler);
 
@@ -67,5 +39,33 @@ export function persist(node: HTMLElement, {
             node.removeEventListener('input', handler);
         },
         update() { }
+    }
+}
+
+function save_form_state(event: Event, store: Writable<any>) {
+    const input = event.target as HTMLInputElement
+    const value = get(store)
+    if (input instanceof HTMLSelectElement) {
+        store.set({ ...value, [input.name]: input.selectedIndex })
+    } else if (input instanceof HTMLInputElement) {
+        store.set({ ...value, [input.name]: input.value })
+    }
+}
+
+const load_cached_values = (node: HTMLElement, store: Writable<any>) => {
+    for (const [key, value] of Object.entries(get(store))) {
+        const inputs = node.querySelectorAll(`[name="${key}"]`)
+        for (const input of inputs) {
+            if (input instanceof HTMLInputElement || input instanceof HTMLSelectElement) {
+                if (input instanceof HTMLInputElement && input.type === 'radio') {
+                    input.checked = input.value === value
+                } else if (input instanceof HTMLSelectElement) {
+                    input.selectedIndex = value as number
+                } else {
+                    // @ts-ignore
+                    input.value = value
+                }
+            }
+        }
     }
 }
--- tests/test.ts
+++ tests/test.ts
@@ -1,11 +1,9 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('forms with use:persist should save their inputs to local storage', async ({ page }) => {
 	await page.goto('/');
 
-	// assert that the page has a form
 	expect(await page.$('form')).toBeTruthy();
 
-	// assert that the form has an input
-	expect(await page.$('form input')).toBeTruthy();
+	const input = await page.$('input');
 });
